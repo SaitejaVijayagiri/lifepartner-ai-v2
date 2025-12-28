d3d8098714dcb90d88c0abf37f3021e26245f627
Saiteja Vijayagiri <saitejavijayagiri@gmail.com>
2025-12-28 10:20:22 +0530

    chore: cleanup debug scripts

--- backend/tests/debug_profile_import.ts
+++
@@ -1,8 +0,0 @@
-
-try {
-    console.log("Attempting to import profile route...");
-    const profile = require('../src/routes/profile');
-    console.log("Success!");
-} catch (e) {
-    console.error("Import Failed:", e);
-}

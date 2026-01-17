d2234753a593a52ce2e66d871972c916289d7ece
Saiteja Vijayagiri <saitejavijayagiri@gmail.com>
2026-01-17 17:06:02 +0530

    Fix: Corrected syntax in robots.ts

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -3,16 +3,18 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: ['/admin/', '/dashboard/'],
-        },
-        {
-        userAgent: 'Googlebot',
-            allow: '/',
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: ['/admin/', '/dashboard/'],
+            },
+            {
+                userAgent: 'Googlebot',
+                allow: '/',
                 disallow: '/private/',
-        }],
-    sitemap: 'https://lifepartnerai.in/sitemap.xml',
+            }
+        ],
+        sitemap: 'https://lifepartnerai.in/sitemap.xml',
     };
 }
